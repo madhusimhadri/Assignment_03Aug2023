79889aa92ea802e5a54125185adf3403785e8edd
Madhu Simhadri <madhunai123@gmail.com>
2023-08-07 23:04:53 +0530

    updated requirement as asked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'weather-app';
+  // tempLow = '/assets/temp1.jpg';
+  public fromChild = 'assets/weatherImage4.jpg';
+
+  updateImage(data: any) {
+    this.fromChild = data.link;
+    console.log(this.fromChild);
+  }
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,30 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent {
-
   city: string = '';
   weather: any;
   errorMsg: string = '';
+  k = 273;
+  tempLow = 'assets/temp1.jpg';
+  tempMid = 'assets/temp2.jpg';
+  tempHigh = 'assets/temp3.jpg';
+
   constructor(private weatherService: WeatherService) {}
 
-  getWeather(){
-    this.weatherService.getWeather(this.city).subscribe((data) =>{
-      this.weather = data;
-      this.errorMsg = '';
-    },
-    (error) =>{
-      console.log("Error in Weather Component: ",error);
-      this.weather=null;
-      this.errorMsg=error;
+  @Output() sendImage = new EventEmitter();
 
-    }
+  getWeather() {
+    this.weatherService.getWeather(this.city).subscribe(
+      (data) => {
+        this.weather = data;
+        this.errorMsg = '';
+        this.weatherDiff();
+      },
+      (error) => {
+        console.log('Error in Weather Component: ', error);
+        this.weather = null;
+        this.errorMsg = error;
+      }
     );
   }
 
+  weatherDiff() {
+    let image = '';
+    if (this.weather.main.temp - this.k < 15) {
+      image = "'/assets/temp1.jpg'";
+    }
+    if (
+      this.weather.main.temp - this.k >= 15 &&
+      this.weather.main.temp - this.k <= 30
+    ) {
+      image = "'/assets/temp2.jpg'";
+    }
+    if (this.weather.main.temp - this.k > 30) {
+      image = "'/assets/temp3.jpg'";
+    }
+
+    this.sendImage.emit({ link: image });
+    console.log('test', image);
+  }
+
+  captureEnter(event: any) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      this.getWeather();
+    }
+  }
 }
